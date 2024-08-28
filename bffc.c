d63c930d712d6643336f723899272e79e57db4bc
verdigris <verdigris1903@proton.me>
2024-08-28 10:11:07 +0300

    feat: add BFF tape evaluation function and memory management in main function

--- bffc.c
+++ bffc.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "bffc.h"
+
+#define MLEN 64
+#define MNUM 256
+#define MAX_EVALS 2**13
+#define BUFFER_SIZE MLEN*2
+
+// Evaluate a BFF tape
+int eval(char* buffer, int bsize) {
+  // Reset read and write heads
+  int head0 = 0;
+  int head1 = 0;
+  int instruct = 0;
+  for (int i = 0; i < bsize; i++) {
+    printf("%c", buffer[i]);
+  }
+  return 0;
+}
 
 int main() {
-  printf("test");
+  char* turing_gas = malloc(MLEN*MNUM*sizeof(char));
+  char* buffer = malloc(MLEN*2*sizeof(char));
+  eval(buffer, BUFFER_SIZE);
+  free(turing_gas);
+  free(buffer);
   return 0;
 }
