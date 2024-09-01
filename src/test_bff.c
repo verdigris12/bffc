2c3b6f4d40efca4386b8f0386c9bf39774b9ceff
verdigris <verdigris1903@proton.me>
2024-09-01 22:52:32 +0300

    feat: add hello world test for brainfuck interpreter

--- src/test_bff.c
+++ src/test_bff.c
@@ -1,16 +1,23 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "bffc.h"
 
-int test_bff_tape1() {
-  char* tape = "12313";
-  eval(tape, strlen(tape));
+int test_bff_hello_world() {
+  // Hello world in brainfuck
+  char* tape = ">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.";
+  const int nbytes = strlen(tape);
+  char * buffer = malloc(nbytes + strlen("Hello world"));
+  memcpy(buffer + strlen("Hello world"), tape, nbytes);
+  eval(buffer, nbytes + strlen("Hello world"));
+  free(buffer);
   return 1;
 }
 
 int main() {
 
-  if (!test_bff_tape1())
+  if (!test_bff_hello_world())
     return 1;
 
   return 0;
